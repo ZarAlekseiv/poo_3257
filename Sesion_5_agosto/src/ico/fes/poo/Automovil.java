67baa1c6a1b79707bbb9d9b05ea1eb35c23da8dd
ZarAlekseiv <alexisvega70@aragon.unam.mx>
2020-08-07 17:21:33 -0500

    clase 7 agosto

--- Sesion_5_agosto/src/ico/fes/poo/Automovil.java
+++ Sesion_5_agosto/src/ico/fes/poo/Automovil.java
@@ -10,23 +10,42 @@ package ico.fes.poo;
  * @author Perro de Tindalo
  */
 public class Automovil {
-    public String marca;
-    public String subMarca;
-    public String modelo;
-    public String color;
+    private String marca;
+    private String subMarca;
+    private int modelo;
+    private String color;
     
+    //metodos de acceso
+    public void setModelo ( int mod){
+        if(mod>= 1914 && mod <= 2020){
+        this.modelo=mod; 
+    }   else {
+        System.out.println("Ese modelo está fuera de rango"); 
+    }
+    }
+    public int getModelo(){
+        return this.modelo;   
+    }
     
     public void encender() {
         System.out.println("Encender el auto");
             }
 
-public void acelerar(){
+    public void acelerar(){
     System.out.println("Acelerando");
         }
-public float avanzar (int metros){
-float reales=0.0f;
-reales =(float) Math.random()*100.0f;
-    System.out.println("Avanzando el auto" + this.marca);
+
+   public float avanzar (int metros){
+    float reales=0.0f;
+    reales =(float) Math.random()*(float)metros;
+    System.out.println("Avanzando el auto " + this.marca);
         return reales;
 }
+    @Override
+    public String toString() {
+        return this.marca + "," + this.subMarca + "," + this.modelo + "," + this.color;
+    }
+
+    
+
         }
